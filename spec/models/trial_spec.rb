36776ab9c174900a76c9ce3914573080ce712ed5
Michael Wenger <michaelwenger27@gmail.com>
2014-07-01 15:59:55 -0400

    Added format documentation to rspec file

--- spec/models/trial_spec.rb
+++ spec/models/trial_spec.rb
@@ -1,5 +1,13 @@
 require "rails_helper"
 
 describe Trial do
+	it "has something going on" do
+		pending
+		trial = Trial.new
+		expect(trial.save).to_have(3).failures
+	end
 
+	it "has something else going on" do
+		pending
+	end
 end
