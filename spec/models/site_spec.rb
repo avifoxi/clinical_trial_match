5147c11163418a3439a81e916a5ed841a9dabc79
Michael Wenger <michaelwenger27@gmail.com>
2014-07-02 19:16:25 -0400

    Added site validation

--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -2,8 +2,7 @@ require "rails_helper"
 
 describe Site do
 	it "is valid with required attributes" do
-		pending('still working on this')
-		fail
+		expect(FactoryGirl.build(:site)).to be_valid
 	end
 
 end
