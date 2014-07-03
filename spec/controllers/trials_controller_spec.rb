fa2ec84e3f207c2c579de3588f6f39c6a6b82dff
Michael Wenger <michaelwenger27@gmail.com>
2014-07-03 15:12:06 -0400

    Added specs for trials order

--- spec/controllers/trials_controller_spec.rb
+++ spec/controllers/trials_controller_spec.rb
@@ -2,14 +2,34 @@ require "rails_helper"
 
 describe TrialsController do
 	describe 'GET #index' do
-		context 'with params' do
-			it "populates an array of contacts starting with the letter"
-			it "renders the :index template"
+		before(:each) do
+			@trial1 = create(:trial, :title => "B title",  :lastchanged_date => 3.days.ago)
+			@trial2 = create(:trial, :title => "A title", :lastchanged_date => 2.days.ago)
+			@trial3 = create(:trial, :title => "C title", :lastchanged_date => 1.days.ago)
+		end
+
+		context 'with order params' do
+			it "populates an array of trials in alphabetical order" do
+				get :index, {:ot => 'title ASC'}
+				expect(assigns(:trials)).to eq([ @trial2, @trial1, @trial3])
+			end
+
+			it "populates an array of trials in date order" do
+				get :index, {:ot => 'lastchanged_date DESC'}
+				expect(assigns(:trials)).to eq([ @trial3, @trial2, @trial1])
+			end
 		end
 
 		context 'without params' do
-			it "populates a paginated array of all trials"
-			it "renders the :index template"
+			it "populates a paginated array of all trials in reverse date order" do
+				get :index
+				expect(assigns(:trials)).to eq([ @trial3, @trial2, @trial1])
+			end
+
+			it "renders the :index template" do
+				get :index
+				expect(response).to render_template :index
+			end
 		end
 	end
 
