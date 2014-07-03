a254ec56d1dd1630a525969bb9dee02042c088cb
Michael Wenger <michaelwenger27@gmail.com>
2014-07-03 16:30:42 -0400

    Added spec for trials and sites to display on import show view

--- spec/controllers/importer_controller_spec.rb
+++ spec/controllers/importer_controller_spec.rb
@@ -5,8 +5,23 @@ describe ImporterController do
 		it "displays the last import" do
 			import = create(:import)
 			get :show
-			# binding.pry
 			expect(assigns(:import)). to eq(import)
+		end
+
+		it "displays all trials count" do
+			trial1 = create(:trial)
+			trial2 = create(:trial)
+			trial3 = create(:trial)
+			get :show
+			expect(assigns(:trials)).to eq [trial1,trial2,trial3]
+		end
+
+		it "displays all sites count" do
+			site1 = create(:site)
+			site2 = create(:site)
+			site3 = create(:site)
+			get :show
+			expect(assigns(:sites)).to eq [site1,site2,site3]
 		end
 
 		it "renders the :show template" do
