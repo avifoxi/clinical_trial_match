704544bff744a7001136c8770be26d3729426026
Michael Wenger <michaelwenger27@gmail.com>
2014-07-03 16:22:57 -0400

    Added import controller specs

--- spec/controllers/importer_controller_spec.rb
+++ spec/controllers/importer_controller_spec.rb
@@ -2,10 +2,18 @@ require "rails_helper"
 
 describe ImporterController do
 	describe 'GET #show' do
-		it "displays the trial, site and import counts" do
+		it "displays the last import" do
+			import = create(:import)
 			get :show
-			binding.pry
+			# binding.pry
+			expect(assigns(:import)). to eq(import)
 		end
+
+		it "renders the :show template" do
+			get :show
+			expect(response).to render_template :show
+		end
+
 	end
 
 	describe 'POST #delete_all' do
