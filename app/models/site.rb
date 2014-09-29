2586d855d6d24b54132f782703ff5534242fd476
Michael Wenger <michaelwenger27@gmail.com>
2014-09-29 17:36:39 -0400

    Removed extraneous comment

--- app/models/site.rb
+++ app/models/site.rb
@@ -2,7 +2,6 @@ class Site < ActiveRecord::Base
 	validates :facility, :status, presence: true
 	belongs_to :trial
 
-	#TODO? City is not populating in the database.
 	geocoded_by :address, :lookup => :yandex
 	after_validation :geocode
 
