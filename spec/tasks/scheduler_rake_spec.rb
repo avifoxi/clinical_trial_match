6b86d891f1c5910786bd8c75e226ef019cafa11d
AviFoxi <frosenox@gmail.com>
2015-02-23 10:40:36 -0500

    basic test for importer:clear_import_date

--- spec/tasks/scheduler_rake_spec.rb
+++ spec/tasks/scheduler_rake_spec.rb
@@ -7,7 +7,20 @@ describe 'importer namespace rake task' do
     Rake::Task.define_task(:environment)
   end
 
-  it 'should not freak the fuck out' do 
+  describe 'importer:clear_import_date' do 
+  	it 'resets import date to 1900, resets trials and sites to 0' do
+  		Rake::Task["importer:clear_import_date"].reenable
+  		Rake.application.invoke_task "importer:clear_import_date"
+  		imp = Import.last
+  		# puts 'import here::::'
+  		# p "#{Import.last.datetime.year.inspect}"
+  		expect(imp.datetime.year).to eq(1900)
+  		expect(imp.valid_trials).to eq(0)
+  		expect(imp.valid_sites).to eq(0)
+  	end
+  end
+
+  describe 'importer:test_email' do 
 
   end
 end
